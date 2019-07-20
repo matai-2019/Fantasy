8384642699500d24f896d4b88163b5e7173dc132
Taine Walford <tainewalford@live.com>
2019-07-20 16:35:55 +1200

    server setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
-const { server, io } = require('./server')
-
+const { server, io, path, app } = require('./server')
+// const { getSession } = require('../client/index')
 const port = process.env.PORT || 3000
 
 io.on('connection', socket => {
+  let userId
   console.log('a user connected')
+  socket.emit('get-state')
+  socket.on('set-state', userState => {
+    userId = userState.id
+    socket.emit('state-loaded', userState.id)
+  })
+  socket.on('state-loaded', () => {
+    if (userId) console.log('Chat-screen')
+    else console.log('Login-screen')
+  })
 })
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
   console.log('Listening on port', port)
+})
+
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, '../public/base.html'))
+})
+
+app.get('/:sessionId', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/app.html'))
 })
