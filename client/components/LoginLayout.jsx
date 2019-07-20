aa286d85686bf99b1194dff203fae0d9f11db0a7
Keith Yoon <uniyoona@naver.com>
2019-07-20 15:43:03 +1200

    adding handlesubmit to login button

--- client/components/LoginLayout.jsx
+++ client/components/LoginLayout.jsx
@@ -1,30 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Container, Button, Icon, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
-const LoginForm = () => (
-  <>
-    <Container>
-      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Message>
-            Welcome to Dungeon &#38; Dragons
-          </Message>
-          <Header as='h2' color='teal' textAlign='center'>
-            <Icon name='grunt' size='large' verticalAlign='middle' />
-            Brace Yourself
-          </Header>
-          <Form size='large'>
-            <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
-              <Button color='teal' fluid size='large'onItemCLick={this.handleItemClick}>
-                Start New Adventure
-              </Button>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    </Container>
-  </>
-)
+class LoginForm extends Component {
+  state = {
+    username: ''
+  }
+
+  handleChange = (e, { username, value }) => this.setState({ [username]: value })
+
+  handleSubmit = () => this.setState({ username: '' })
+
+  render () {
+    const { username } = this.state
+    return (
+      <Container>
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Message>
+              Welcome to Dungeon &#38; Dragons
+            </Message>
+            <Header as='h2' color='teal' textAlign='center'>
+              <Icon name='grunt' size='large' />
+              Brace Yourself
+            </Header>
+            <Form onSubmit={this.handleSubmit} size='large'>
+              <Segment stacked>
+                <Form.Input
+                  fluid icon='user'
+                  iconPosition='left'
+                  defaultValue={username}
+                  onChange={this.handleChange}
+                  placeholder='Username' />
+                <Button color='teal' fluid size='large' type='submit'>
+                  Start New Adventure
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    )
+  }
+}
 
 export default LoginForm
