349dc68517644b4838030a063d0f3bdfa4643828
Taine Walford <tainewalford@live.com>
2019-07-22 00:41:20 +1200

    changed list to map

--- client/components/ChatLayout.jsx
+++ client/components/ChatLayout.jsx
@@ -10,112 +10,25 @@ export const ChatTemplate = ({ userArray, messageArray }) => {
       <Grid columns={2} relaxed='very'>
         <Grid.Column floated="left" width={6}>
           <List divided relaxed>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column>
-                  <List.Icon name='grunt' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>User-1</List.Header>
-                    <List.Description as='a'>Updated 10 mins ago</List.Description>
-                  </List.Content>
-                </Grid.Column>
-                <Grid.Column floated='right' width={3}>
-                  <Segment compact>
-                    <Checkbox />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column>
-                  <List.Icon name='grunt' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>User-2</List.Header>
-                    <List.Description as='a'>Updated 22 mins ago</List.Description>
-                  </List.Content>
-                </Grid.Column>
-                <Grid.Column floated='right' width={3}>
-                  <Segment compact>
-                    <Checkbox />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column>
-                  <List.Icon name='grunt' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>User-3</List.Header>
-                    <List.Description as='a'>Updated 34 mins ago</List.Description>
-                  </List.Content>
-                </Grid.Column>
-                <Grid.Column floated='right' width={3}>
-                  <Segment compact>
-                    <Checkbox />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column>
-                  <List.Icon name='grunt' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>User-4</List.Header>
-                    <List.Description as='a'>Updated 34 mins ago</List.Description>
-                  </List.Content>
-                </Grid.Column>
-                <Grid.Column floated='right' width={3}>
-                  <Segment compact>
-                    <Checkbox />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column>
-                  <List.Icon name='grunt' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>User-5</List.Header>
-                    <List.Description as='a'>Updated 34 mins ago</List.Description>
-                  </List.Content>
-                </Grid.Column>
-                <Grid.Column floated='right' width={3}>
-                  <Segment compact>
-                    <Checkbox />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column>
-                  <List.Icon name='grunt' size='large' verticalAlign='middle' />
-                  <List.Content>
-                    <List.Header as='a'>User-6</List.Header>
-                    <List.Description as='a'>Updated 34 mins ago</List.Description>
-                  </List.Content>
-                </Grid.Column>
-                <Grid.Column floated='right' width={3}>
-                  <Segment compact>
-                    <Checkbox />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            </List.Item >
-          </List >
-        </Grid.Column >
-        <Grid.Column>
-          <List>
-            <List.Item>
-              <Grid columns={2} relaxed='very'>
-                <Grid.Column floated="right" width={6}>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
+            {this.messageArray.map(user => {
+              return <>
+                <List.Item>
+                  <Grid columns={2} relaxed='very'>
+                    <Grid.Column>
+                      <List.Icon name='grunt' size='large' verticalAlign='middle' />
+                      <List.Content>
+                        <List.Header as='a'>{user.userName}</List.Header>
+                      </List.Content>
+                    </Grid.Column>
+                    <Grid.Column floated='right' width={3}>
+                      <Segment compact>
+                        <Checkbox />
+                      </Segment>
+                    </Grid.Column>
+                  </Grid>
+                </List.Item>
+              </>
+            })}
           </List>
         </Grid.Column >
       </Grid >
