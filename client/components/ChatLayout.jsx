371b32ecd0e5dd6d23f55fb18aeb94ecbeacda7f
AndreD4 <andremai629@gmail.com>
2019-07-23 12:21:00 +1200

    ADDING CHANGES

--- client/components/ChatLayout.jsx
+++ client/components/ChatLayout.jsx
@@ -13,6 +13,117 @@ export const ChatTemplate = ({ userArray, messageArray, sendMessage }) => {
   }
 
   return <>
+<<<<<<< HEAD
+    {console.log('render', userArray)}
+    {console.log(messageArray)}
+    <Container>
+      <Segment>
+        <div>
+          <Container>
+            <div>
+              <Grid columns={2} relaxed='very'>
+                <Grid.Column floated="left" width={6}>
+                  <List divided relaxed>
+                    {userArray.map(user => {
+                      return <>
+                        <List.Item>
+                          <Grid columns={2} relaxed='very'>
+                            <Grid.Column>
+                              <List.Header as='a'>{user.userName}</List.Header>
+                            </Grid.Column>
+                            <Grid.Column floated='right' width={3}>
+
+                              <Checkbox />
+
+                            </Grid.Column>
+                          </Grid>
+                        </List.Item>
+                      </>
+                    })}
+                  </List>
+                </Grid.Column >
+                <Grid.Column>
+                  <List divided relaxed>
+                    {messageArray.map(message => {
+                      return <>
+                        {/* <List.Item>
+                          <List.Content>
+                            <List.Header>{message.userName}</List.Header>
+                            <List.Description>{message.message}</List.Description>
+                          </List.Content>
+                        </List.Item> */}
+                      </>
+                    })}
+                  </List>
+                </Grid.Column>
+              </Grid >
+            </div >
+            <Segment.Group>
+              <Button floated='left' positive>Admin Options</Button>
+              <div floated='right'>
+                <Input type='text' id="messageInput" placeholder='Your message goes here...' onChange={handleChange} />
+                <Button type='submit' onClick={handleSend}>Send</Button>
+              </div>
+            </Segment.Group>
+          </Container>
+        </div>
+      </Segment>
+    </Container>
+  </>
+||||||| merged common ancestors
+  { console.log('render', userArray)}
+  { console.log(messageArray) }
+  <Container>
+    <div>
+      <Grid columns={2} relaxed='very'>
+        <Grid.Column floated="left" width={6}>
+          <List divided relaxed>
+            {userArray.map(user => {
+              return <>
+                <List.Item>
+                  <Grid columns={2} relaxed='very'>
+                    <Grid.Column>
+                      <List.Content>
+                        <List.Header as='a'>{user.userName}</List.Header>
+                      </List.Content>
+                    </Grid.Column>
+                    <Grid.Column floated='right' width={3}>
+                      <Segment compact>
+                        <Checkbox />
+                      </Segment>
+                    </Grid.Column>
+                  </Grid>
+                </List.Item>
+              </>
+            })}
+          </List>
+        </Grid.Column >
+        <Grid.Column>
+          <List divided relaxed>
+            {messageArray.map(message => {
+              return <>
+                <List.Item>
+                  <List.Content>
+                    <List.Header>{message.userName}</List.Header>
+                    <List.Description>{message.message}</List.Description>
+                  </List.Content>
+                </List.Item>
+              </>
+            })}
+          </List>
+        </Grid.Column>
+      </Grid >
+    </div >
+    <Segment.Group horizontal>
+      <Button floated='left' positive>Admin Options</Button>
+      <div floated='right'>
+        <Input type='text' id="messageInput" placeholder='Your message goes here...' onChange={handleChange} />
+        <Button type='submit' onClick={handleSend}>Send</Button>
+      </div>
+    </Segment.Group>
+  </Container>
+  </>
+=======
   <Container>
     <div>
       <Modal trigger={<Button floated="right" animated='vertical' color='violet'>
@@ -126,9 +237,10 @@ export const ChatTemplate = ({ userArray, messageArray, sendMessage }) => {
             <List divided relaxed>
               {messageArray.map(message => {
                 return <div key={message.timestamp}>
-                  <Segment style={{overflow: 'auto', maxHeight: 200 }}>
-                    {message.userName},{message.messageText}
-                    </Segment>
+                  <Segment.Group horizontal>
+                    <Segment compact>{message.userName}</Segment>
+                    <Segment compact>{message.messageText}</Segment>
+                  </Segment.Group>
                 </div>
               })}
             </List>
@@ -142,4 +254,5 @@ export const ChatTemplate = ({ userArray, messageArray, sendMessage }) => {
   </Container>
   <br></br>
     </>
+>>>>>>> 545083ec0a1c328ba39cff6db38c96f7b7fde5ba
 }
