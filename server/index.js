c01a52474f0e1d7f7769144b6ce6ec18e6c7b527
Taine Walford <tainewalford@live.com>
2019-08-11 13:06:52 +1200

    added fs config and fn removeUser

--- server/index.js
+++ server/index.js
@@ -1,23 +1,43 @@
 const { server, io, path, app } = require('./server')
+
+const firebase = require('firebase/app')
+require('firebase/firestore')
+require('dotenv')
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: 'fantasy-scroll',
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MSG_SEND_ID,
+  appId: process.env.APP_ID
+}
+const fire = firebase.initializeApp(firebaseConfig)
+const db = fire.firestore()
+
 const port = process.env.PORT || 3000
 
 io.on('connection', socket => {
-  let userId
-  socket.emit('load-user')
+  let userId, ssID
   socket.on('set-state', userState => {
     userId = userState.id
-    io.emit('pull-users')
-    io.emit('pull-messages')
+    ssID = userState.ssID
   })
   socket.on('change-occured', () => {
     io.emit('update-sockets')
   })
   socket.on('disconnect', () => {
+    io.emit('dc-user', userId)
+    console.log('DC USID', userId, typeof userId)
+    console.log('DC SSID', ssID, typeof ssID)
+    if (userId === undefined || userId === null) console.log('no user')
+    else console.log('remove user on dc')// removeUser(ssID, userId)
+    socket.emit('clear-ss')
   })
 })
 
 server.listen(port, function () {
-  // eslint-disable-next-line no-console
+  // eslint-disable-next-line no-consoleq
   console.log('Listening on port', port)
 })
 
@@ -27,3 +47,20 @@ app.get('/', (req, res) => {
 app.get('/**', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'))
 })
+
+const removeUser = (sessionId, Userid) => {
+  return db.collection(sessionId).doc('Users').get()
+    .then(obj => {
+      obj = obj.data()
+      const removed = obj.users.filter(user => user.id === Userid)[0]
+      const index = obj.users.indexOf(removed)
+      if (index !== -1) obj.users.splice(index, 1)
+      return obj
+    })
+    .then((obj) => {
+      return db.collection(sessionId).doc('Users').set(obj)
+        .then(() => {
+          return obj
+        })
+    })
+}
